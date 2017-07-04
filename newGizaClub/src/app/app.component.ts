051c29e5836e573f88c2cab70ef0372159e5e636
EdgeTechn/Mohammed <mohammed.magdy.ali.96@gmail.com>
2017-07-04 16:23:06 +0200

    added : side menu

--- newGizaClub/src/app/app.component.ts
+++ newGizaClub/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import {Nav,Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -9,19 +9,32 @@ import { LandingPage} from '../pages/landing/landing';
 import {RegestrationPage } from '../pages/regestration/regestration';
 import { LoginPage } from '../pages/login/login';
 
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+   @ViewChild(Nav) nav: Nav;
   rootPage:any = LoginPage ;
-
+  pages: Array<{title: string, component: any}>;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.pages=[
+      {title: "Home" ,component : HomePage},
+      {title: 'LandingPage' ,component :LandingPage},
+      {title: 'RegestrationPage' ,component :RegestrationPage},
+      {title: 'LoginPage' ,component : LoginPage}
+    ]
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
   }
   
   
