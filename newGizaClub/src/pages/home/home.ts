42652716f57fae10e3ae58f3a8d65760fc83ed91
EdgeTechn/Mohammed <mohammed.magdy.ali.96@gmail.com>
2017-07-17 16:57:26 +0200

    added sms verification

--- newGizaClub/src/pages/home/home.ts
+++ newGizaClub/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import {SMS} from '@ionic-native/sms';
 
 import{ FacilitieslistPage} from '../facilitieslist/facilitieslist';
 import{ SportslistPage} from '../sportslist/sportslist';
@@ -12,8 +13,14 @@ export class HomePage {
    public facilities= FacilitieslistPage;
    public sports= SportslistPage;
 
-  constructor () {
- 
+  constructor (private sms :SMS) {
+    
+    //if(this.sms.hasPermission()){
+      //this.sms.send('01099297597','this is a test massage');
+    //  alert("we have permission to send sms");
+   // }else{
+    //  alert("we don`t have permission to send sms");
+   // }
   }
 
 }
